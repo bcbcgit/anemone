b6a6c2c5cd86d454610b111215d30714eb0de585
rize <tkworks@rize.red>
2025-09-03 16:42:31 +0900

    NoIndex

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="utf-8">
+    <meta name="robots" content="noindex, nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'ログイン')</title>
